834b7a0e21a3cfb4d4aec4e4dbc46d893654277b
João Vitor Borges <joao-salves@hotmail.com>
2021-03-16 20:44:37 -0300

    Desafio concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found' });
+  }
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'User exceed non pro quantity of todos' });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found' });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Invalid id' });
+  }
+
+  const userTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!userTodo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.user = user;
+  request.todo = userTodo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found' });
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
